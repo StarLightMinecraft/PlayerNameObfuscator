dfb6bfd3b7707955a7e1e04c8ed715f5da57828e
HikariLan <shaokeyibb@126.com>
2022-06-25 23:07:36 +0800

    base64 to hashcode

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A StarLight Minecraft Server project by HikariLan
 
-PlayerNameObfuscator can be used to obfuscate player names (it will be obfuscated by using Base64).
+PlayerNameObfuscator can be used to obfuscate player names (it will be obfuscated by using hashcode).
 
 ## Environment
 
--- src/main/java/io/hikarilan/playernameobfuscator/PlayerNameObfuscator.java
+++ src/main/java/io/hikarilan/playernameobfuscator/PlayerNameObfuscator.java
@@ -1,15 +1,14 @@
 package io.hikarilan.playernameobfuscator;
 
 import com.google.inject.Inject;
-import com.velocitypowered.api.event.player.GameProfileRequestEvent;
-import com.velocitypowered.api.event.player.ServerConnectedEvent;
-import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.player.GameProfileRequestEvent;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import org.slf4j.Logger;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
+import java.util.Arrays;
 
 @Plugin(
         id = "playernameobfuscator",
@@ -30,7 +29,7 @@ public class PlayerNameObfuscator {
     @Subscribe
     public void onRequestGameProfile(GameProfileRequestEvent e) {
         String name = e.getUsername();
-        String encode = Base64.getEncoder().encodeToString(name.getBytes(StandardCharsets.UTF_8));
+        String encode = Arrays.hashCode(name.getBytes(StandardCharsets.UTF_8)) + "";
         e.setGameProfile(e.getGameProfile().withName(encode.substring(0, Math.min(encode.length() - 1, 15))));
         logger.info("Player name obfuscated: " + e.getGameProfile().getName() + "(" + name + ")");
     }
